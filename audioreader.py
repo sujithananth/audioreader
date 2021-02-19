13efb6c018f40bdc342ffa7e892f2bed219d9fb2
sujith ananth <www.sujithananth20@gmail.com>
2021-02-19 12:26:57 +0530

    created the exe file

--- audioreader.py
+++ audioreader.py
@@ -7,10 +7,12 @@ if (ad=="yes" )or (ad=="YES"):
     bn=input("enter the name with .pdf :",)
     bookshelf.append(bn)
 else:
-    None  
-bookname=input("enter the book name :",)
-speaker=pyttsx3.init() 
-def audioreader():
+    print("~~~~~~~~~~~~~~~~~~") 
+bookname=str(input("enter the book name with .pdf:",))
+speaker=pyttsx3.init()
+    
+z=str(input("what u want to do  'read' or 'write to mp3'>>"))
+if (z=='read') or (z=='READ'):
     book = open (bookname,'rb') #opening the book in binary to be read  by python pyPDF2
     pdfreader = PyPDF2.PdfFileReader(book)
     pages=pdfreader.numPages
@@ -20,14 +22,18 @@ def audioreader():
     number=wh-1
     page=pdfreader.getPage(number)  #gets page no from which it has to read
     text=page.extractText() #extracting texts from the pdf
-    return text
-z=str(input("what u want to do  'read' or 'write to mp3'>>"))
-if (z=='read') or (z=='READ'):
-    text=audioreader()
     speaker.say(text)
     speaker.runAndWait()
 elif (z=='write to mp3') or(z=='write'):
-    text=audioreader()
+    book = open (bookname,'rb') #opening the book in binary to be read  by python pyPDF2
+    pdfreader = PyPDF2.PdfFileReader(book)
+    pages=pdfreader.numPages
+    print("total no of pages :",pages)
+    speaker.setProperty('rate', 90)#speed at which the reader speaks
+    wh=int(input("enter which page u want:"))
+    number=wh-1
+    page=pdfreader.getPage(number)  #gets page no from which it has to read
+    text=page.extractText() #extracting texts from the pdf
     mpname=str(input("enter how to save the mp3 file>>"))
     speaker.save_to_file(text,mpname+".mp3")
     speaker.runAndWait()
