044de51577eb057414b0553318dbab3509211b06
sujith ananth <www.sujithananth20@gmail.com>
2021-04-09 13:17:15 +0530

    basic

--- webscrap_gui.py
+++ webscrap_gui.py
@@ -1,10 +1,10 @@
 #webscrap
 import PySimpleGUI as sg
-import webscrap_backend
 sg.theme('darkamber')
 layout=[
+    [sg.InputText('',size=(30,10),font=('courier',16),key='book name'),sg.Button("enter book name")],
     [sg.InputText('',size=(70,2),key='url'),sg.Button("get data",font=('courier',20),key='get',bind_return_key=True)],
-    [sg.Multiline('',size=(50,15),font=('courier',16),key='out',disabled=True)]
+    
 ]
 def  display_analytics(url):
      html_page=get_html_content(url)
